909f3817022dd23c99f6bba394717585fb7d051d
Yulia Levochkina <levochkinajuli@yandex.ru>
2023-10-16 18:48:56 +0300

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # qa_python
+==================================================== test session starts =====================================================
+platform win32 -- Python 3.11.5, pytest-7.4.2, pluggy-1.3.0 -- C:\Users\Юлия\AppData\Local\Programs\Python\Python311\python.exe
+cachedir: .pytest_cache
+rootdir: C:\Users\Юлия\qa_python
+plugins: cov-4.1.0
+collected 11 items                                                                                                             
+
+tests.py::TestBooksCollector::test_add_one_book_without_genre_true PASSED                                               [  9%] 
+tests.py::TestBooksCollector::test_set_book_genre_true PASSED                                                           [ 18%] 
+tests.py::TestBooksCollector::test_get_added_book_genre_true PASSED                                                     [ 27%] 
+tests.py::TestBooksCollector::test_get_books_with_specific_genre_zero_added_books_true PASSED                           [ 36%] 
+tests.py::TestBooksCollector::test_get_books_genre_one_book_and_genre_added PASSED                                      [ 45%] 
+tests.py::TestBooksCollector::test_in_books_for_children_no_books_for_adults PASSED                                     [ 54%] 
+tests.py::TestBooksCollector::test_add_book_in_favorites_true[\u0413\u0435\u043d\u0438\u0439 \u0438 \u0437\u043b\u043e\u0434\u0435\u0439\u0441\u0442\u0432\u043e] PASSED [ 63%]
+tests.py::TestBooksCollector::test_add_book_in_favorites_true[\u0412\u0430\u0440\u0448\u0430\u0432\u0430] PASSED        [ 72%] 
+tests.py::TestBooksCollector::test_add_book_in_favorites_true[\u041c\u0438\u0445\u0430\u0438\u043b \u0413\u0440\u0438\u0433\u043e\u0440\u044c\u0435\u0432\u0438\u0447] PASSED [ 81%]
+tests.py::TestBooksCollector::test_delete_book_from_favorites_true PASSED                                               [ 90%] 
+tests.py::TestBooksCollector::test_get_list_of_favorites_books_true PASSED                                              [100%] 
+
+===================================================== 11 passed in 0.05s ===================================================== 
--- tests.py
+++ tests.py
@@ -1,24 +1,69 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+import pytest
+
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    def test_add_one_book_without_genre_true(self):
         collector = BooksCollector()
+        collector.add_new_book('Чебурашка')
+        assert 'Чебурашка' in collector.books_genre and collector.books_genre['Чебурашка'] == ''
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_set_book_genre_true(self):
+        collector_1 = BooksCollector()
+        book_name = 'Блеск и нищета куртизанок'
+        book_genre = 'Мультфильмы'
+        collector_1.add_new_book(book_name)
+        collector_1.set_book_genre(book_name, book_genre)
+        assert collector_1.books_genre[book_name] != ''
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_get_added_book_genre_true(self):
+        collector_2 = BooksCollector()
+        book_name = 'Блеск и нищета куртизанок'
+        book_genre = 'Мультфильмы'
+        collector_2.add_new_book(book_name)
+        collector_2.set_book_genre(book_name, book_genre)
+        assert collector_2.get_book_genre(book_name) == book_genre
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_get_books_with_specific_genre_zero_added_books_true(self):
+        collector_3 = BooksCollector()
+        detectives = collector_3.get_books_with_specific_genre('Детективы')
+        assert len(detectives) == 0
+
+    def test_get_books_genre_one_book_and_genre_added(self):
+        collector_4 = BooksCollector()
+        collector_4.add_new_book('Бусинка')
+        collector_4.set_book_genre('Бусинка', 'Детектив')
+        assert 'Бусинка', 'Детектив' in collector_4.get_books_genre()
+
+
+    def test_in_books_for_children_no_books_for_adults(self):
+        collector_5 = BooksCollector()
+        collector_5.add_new_book('Golf')
+        collector_5.set_book_genre('Golf', 'Ужасы')
+        assert len(collector_5.get_books_for_children()) == 0
+
+    @pytest.mark.parametrize('name', ['Гений и злодейство', 'Варшава', 'Михаил Григорьевич'])
+    def test_add_book_in_favorites_true(self, name):
+        collector_9 = BooksCollector()
+        collector_9.add_new_book(name)
+        collector_9.add_book_in_favorites(name)
+        assert collector_9.favorites[0] == name
+
+    def test_delete_book_from_favorites_true(self):
+        collector_7 = BooksCollector()
+        collector_7.add_new_book('Доктор Живаго')
+        collector_7.add_book_in_favorites('Доктор Живаго')
+        collector_7.add_new_book('Доктор')
+        collector_7.add_book_in_favorites('Доктор')
+        collector_7.delete_book_from_favorites('Доктор')
+        assert len(collector_7.favorites) == 1
+
+    def test_get_list_of_favorites_books_true(self):
+        collector_8 = BooksCollector()
+        collector_8.add_new_book('Красная шапочка')
+        collector_8.add_book_in_favorites('Красная шапочка')
+        collector_8.add_new_book('Весна')
+        collector_8.add_book_in_favorites('Весна')
+        assert 'Красная шапочка', 'Весна' in collector_8.favorites
