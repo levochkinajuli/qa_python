613904f3adce12b693400a650d459465325d0734
Yulia Levochkina <levochkinajuli@yandex.ru>
2023-10-17 13:31:04 +0300

    commit after comments

--- tests.py
+++ tests.py
@@ -5,65 +5,64 @@ import pytest
 
 class TestBooksCollector:
 
-    def test_add_one_book_without_genre_true(self):
+    def test_add_new_book_add_one_book(self):
         collector = BooksCollector()
         collector.add_new_book('Чебурашка')
         assert 'Чебурашка' in collector.books_genre and collector.books_genre['Чебурашка'] == ''
 
-    def test_set_book_genre_true(self):
-        collector_1 = BooksCollector()
+    def test_set_book_genre_set_one_book_genre(self):
+        collector = BooksCollector()
         book_name = 'Блеск и нищета куртизанок'
         book_genre = 'Мультфильмы'
-        collector_1.add_new_book(book_name)
-        collector_1.set_book_genre(book_name, book_genre)
-        assert collector_1.books_genre[book_name] != ''
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, book_genre)
+        assert collector.books_genre[book_name] == book_genre
 
-    def test_get_added_book_genre_true(self):
-        collector_2 = BooksCollector()
+    def test_get_books_genre_one_book_added(self):
+        collector = BooksCollector()
         book_name = 'Блеск и нищета куртизанок'
         book_genre = 'Мультфильмы'
-        collector_2.add_new_book(book_name)
-        collector_2.set_book_genre(book_name, book_genre)
-        assert collector_2.get_book_genre(book_name) == book_genre
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, book_genre)
+        assert collector.get_book_genre(book_name) == book_genre
 
-    def test_get_books_with_specific_genre_zero_added_books_true(self):
-        collector_3 = BooksCollector()
-        detectives = collector_3.get_books_with_specific_genre('Детективы')
-        assert len(detectives) == 0
+    def test_get_books_with_specific_genre_zero_added_books(self):
+        collector = BooksCollector()
+        assert len(collector.get_books_with_specific_genre('Детективы')) == 0
 
     def test_get_books_genre_one_book_and_genre_added(self):
-        collector_4 = BooksCollector()
-        collector_4.add_new_book('Бусинка')
-        collector_4.set_book_genre('Бусинка', 'Детектив')
-        assert 'Бусинка', 'Детектив' in collector_4.get_books_genre()
+        collector = BooksCollector()
+        collector.add_new_book('Бусинка')
+        collector.set_book_genre('Бусинка', 'Детектив')
+        assert 'Бусинка', 'Детектив' in collector.get_books_genre()
 
 
-    def test_in_books_for_children_no_books_for_adults(self):
-        collector_5 = BooksCollector()
-        collector_5.add_new_book('Golf')
-        collector_5.set_book_genre('Golf', 'Ужасы')
-        assert len(collector_5.get_books_for_children()) == 0
+    def test_get_books_for_children_no_books_added(self):
+        collector = BooksCollector()
+        collector.add_new_book('Golf')
+        collector.set_book_genre('Golf', 'Ужасы')
+        assert len(collector.get_books_for_children()) == 0
 
     @pytest.mark.parametrize('name', ['Гений и злодейство', 'Варшава', 'Михаил Григорьевич'])
-    def test_add_book_in_favorites_true(self, name):
-        collector_9 = BooksCollector()
-        collector_9.add_new_book(name)
-        collector_9.add_book_in_favorites(name)
-        assert collector_9.favorites[0] == name
+    def test_add_book_in_favorites_one_book_added(self, name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.add_book_in_favorites(name)
+        assert collector.favorites[0] == name
 
-    def test_delete_book_from_favorites_true(self):
-        collector_7 = BooksCollector()
-        collector_7.add_new_book('Доктор Живаго')
-        collector_7.add_book_in_favorites('Доктор Живаго')
-        collector_7.add_new_book('Доктор')
-        collector_7.add_book_in_favorites('Доктор')
-        collector_7.delete_book_from_favorites('Доктор')
-        assert len(collector_7.favorites) == 1
+    def test_delete_book_from_favorites_one_book_deleted(self):
+        collector = BooksCollector()
+        collector.add_new_book('Доктор Живаго')
+        collector.add_book_in_favorites('Доктор Живаго')
+        collector.add_new_book('Доктор')
+        collector.add_book_in_favorites('Доктор')
+        collector.delete_book_from_favorites('Доктор')
+        assert len(collector.favorites) == 1
 
-    def test_get_list_of_favorites_books_true(self):
-        collector_8 = BooksCollector()
-        collector_8.add_new_book('Красная шапочка')
-        collector_8.add_book_in_favorites('Красная шапочка')
-        collector_8.add_new_book('Весна')
-        collector_8.add_book_in_favorites('Весна')
-        assert 'Красная шапочка', 'Весна' in collector_8.favorites
+    def test_get_list_of_favorites_books_two_books_added(self):
+        collector = BooksCollector()
+        collector.add_new_book('Красная шапочка')
+        collector.add_book_in_favorites('Красная шапочка')
+        collector.add_new_book('Весна')
+        collector.add_book_in_favorites('Весна')
+        assert len(collector.favorites) == 2
